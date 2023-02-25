be2e7c02127e51cdc5b8b1903502e39c5ae38a42
Mahta Jannatifar <jannatif@usc.edu>
2023-02-24 18:30:53 -0800

    create table1

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -1,3 +1,9 @@
+import com.apple.foundationdb.Database;
+import com.apple.foundationdb.FDB;
+import com.apple.foundationdb.directory.DirectoryLayer;
+import com.apple.foundationdb.directory.DirectorySubspace;
+import com.apple.foundationdb.directory.PathUtil;
+
 import java.util.HashMap;
 
 /**
@@ -12,6 +18,49 @@ public class TableManagerImpl implements TableManager{
     // your code
 
     System.out.println("blabla---");
+    if(tableName == null){
+      return StatusCode.TABLE_CREATION_ATTRIBUTE_INVALID;
+    }
+    else if(attributeNames == null){
+      return  StatusCode.TABLE_CREATION_ATTRIBUTE_NAME_INVALID;
+    }
+    else if(attributeType == null){
+      return  StatusCode.TABLE_CREATION_ATTRIBUTE_INVALID;
+    }
+    else if(primaryKeyAttributeNames == null){
+      return  StatusCode.TABLE_CREATION_NO_PRIMARY_KEY;
+    }
+    else if(attributeNames.length != attributeType.length){
+      return StatusCode.TABLE_CREATION_DIFFERENT_SIZES;
+    }
+    else{
+      FDB fdb = FDB.selectAPIVersion(710);
+      Database db = null;
+      DirectorySubspace rootDirectory = null;
+
+      try {
+        db = fdb.open();
+      } catch (Exception e) {
+        System.out.println("ERROR: the database is not successfully opened: " + e);
+      }
+
+      System.out.println("Open FDB Successfully!");
+      // initialize root directory, which stands for the Company
+      try {
+        rootDirectory = DirectoryLayer.getDefault().createOrOpen(db,
+                PathUtil.from("root")).join();
+      } catch (Exception e) {
+        System.out.println("ERROR: the root directory is not successfully opened: " + e);
+      }
+      System.out.println("Created root directory successfully!");
+//    now can create table:
+      DirectorySubspace dir = null;
+      dir = DirectoryLayer.getDefault().createOrOpen(db,PathUtil.from(tableName)).join();
+      if(DirectoryLayer.DEFAULT_NODE_SUBSPACE.contains(tableName.getBytes())){
+        System.out.println("table exists");
+      }
+    }
+
     return StatusCode.SUCCESS;
   }
 
