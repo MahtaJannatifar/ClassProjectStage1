f6826c71e5de4814dd32eb8ffd6ca6881f377cb1
Mahta Jannatifar <jannatif@usc.edu>
2023-03-01 22:07:08 -0800

    insert success

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -57,8 +57,6 @@ public class TableManagerImpl implements TableManager{
       } catch (Exception e) {
         System.out.println("ERROR: the database is not successfully opened: " + e);
       }
-
-//      System.out.println("Open FDB Successfully!");
       // initialize root directory, which stands for the Company
       try {
         rootDirectory = DirectoryLayer.getDefault().createOrOpen(db,
@@ -66,19 +64,17 @@ public class TableManagerImpl implements TableManager{
       } catch (Exception e) {
         System.out.println("ERROR: the root directory is not successfully opened: " + e);
       }
-//      System.out.println("Created root directory successfully!");
       // if the subdirectory does not exist, add it
       // initialize two subdirectories under the company, Employee and Department
       final DirectorySubspace subdir = rootDirectory.createOrOpen(db, PathUtil.from(tableName)).join();
       Transaction tx = db.createTransaction();
 
-//      System.out.println("DirectoryLayer: " + DirectoryLayer.getDefault().list(tx).join()+" trying to add " + tableName);
       if( DirectoryLayer.getDefault().list(tx).join().contains(tableName)) {
         System.out.println(tableName+"  already exists,cannot create a table with existing name!");
         return StatusCode.SUCCESS;
       }
       else{
-        System.out.println(tableName+" does not exist");
+        System.out.println(tableName+" does not exist, going to add to table.");
 
         //need to add the table to fdb:
         for (int i=0; i< DirectoryLayer.getDefault().list(tx).join().size(); i++) {
@@ -105,9 +101,19 @@ public class TableManagerImpl implements TableManager{
 
   @Override
   public HashMap<String, TableMetadata> listTables() {
-    System.out.println("list table was called");
+
     // your code
-    return null;
+    FDB fdb = FDB.selectAPIVersion(710);
+    Database db = null;
+    DirectorySubspace rootDirectory = null;
+    try {
+      db = fdb.open();
+    } catch (Exception e) {
+      System.out.println("ERROR: the database is not successfully opened: " + e);
+    }
+    Transaction tx = db.createTransaction();
+    System.out.println("list table was called" + (HashMap<String, TableMetadata>) DirectoryLayer.getDefault().list(tx).join());
+    return (HashMap<String, TableMetadata>) DirectoryLayer.getDefault().list(tx).join();
   }
 
   @Override
--- src/TableManagerTest.java
+++ src/TableManagerTest.java
@@ -50,8 +50,8 @@ public class TableManagerTest {
         EmployeeTableAttributeNames, EmployeeTableAttributeTypes, EmployeeTablePKAttributes));
 
 //    todo: cannot use w/o implementing the listTable()
-//    HashMap<String, TableMetadata> tables = tableManager.listTables();
-//    assertEquals(1, tables.size());
+    HashMap<String, TableMetadata> tables = tableManager.listTables();
+    assertEquals(1, tables.size());
 //    assertEquals(EmployeeTable, tables.get(EmployeeTableName));
   }
 
