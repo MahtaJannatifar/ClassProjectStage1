828c0e61fb1d8b1befa6d7789ffc86171e7c548c
Mahta Jannatifar <jannatif@usc.edu>
2023-03-01 21:52:15 -0800

    insert success

--- src/TableManagerTest.java
+++ src/TableManagerTest.java
@@ -50,8 +50,7 @@ public class TableManagerTest {
         EmployeeTableAttributeNames, EmployeeTableAttributeTypes, EmployeeTablePKAttributes));
 
     HashMap<String, TableMetadata> tables = tableManager.listTables();
-    System.out.println("table size===> "+tables.size());
-//    assertEquals(1, tables.size());
+    assertEquals(1, tables.size());
 //    assertEquals(EmployeeTable, tables.get(EmployeeTableName));
   }
 
