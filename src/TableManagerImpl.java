a0441500d56dd99f3d3a5539ee8a4472ca042afe
Mahta Jannatifar <jannatif@usc.edu>
2023-03-05 15:15:18 -0800

    insert success

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -135,7 +135,7 @@ public class TableManagerImpl implements TableManager{
 
 
       System.out.println(tableName+" SUB DIR Get KEY: "+ Arrays.toString(k.pack()));
-      System.out.println(tableName+" SUB DIR Get VALUE: "+ Tuple.from(k.pack()).pack());
+      System.out.println(tableName+" SUB DIR Get VALUE: "+ Arrays.toString(Tuple.from(k.pack()).pack()));
 
       atrNameList.add(key);
       typesList.add(Tuple.from(k).pack());
