651fe0f590da2d3c53d09362003a0bddc1269b24
Mahta Jannatifar <jannatif@usc.edu>
2023-03-05 20:18:36 -0800

    insert success

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -144,7 +144,7 @@ public class TableManagerImpl implements TableManager{
 
       Tuple k = Tuple.from(tableName);
 //      todo: change these values just need to find syntax to fetch!
-      Object record = Tuple.from(kvs).get(i);
+      Object record = Tuple.from(Tuple.from(kvs));
       System.out.println("Record: "+record);
       String atrName = "";
       boolean isPK = true;
