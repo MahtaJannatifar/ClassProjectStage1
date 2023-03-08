a658f5324761a0d69cd8e49efd59216774936643
Mahta Jannatifar <jannatif@usc.edu>
2023-03-07 18:21:44 -0800

    insert success

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -149,10 +149,10 @@ public class TableManagerImpl implements TableManager{
         Tuple keyTuple = dir.unpack(kvs.get(k).getKey());
         System.out.println("keyTuple: "+ keyTuple);
         Tuple valueTuple = Tuple.from( kvs.get(k).getValue());
-        System.out.println("ValueTuple: "+  valueTuple.get(k));
+        System.out.println("ValueTuple: "+  valueTuple);
         //boolean isPK = (boolean) valueTuple.getItems().get(0);
         //AttributeType attrType = (AttributeType) valueTuple.getItems().get(1);
-        System.out.println(valueTuple.get(0).toString() + " tupleFirst");
+        System.out.println((Boolean) valueTuple.get(0) + " tupleFirst");
 //        if(isPK){
 //
 //          primKeysList.add(keyTuple.get(0).toString());
